50c791542b80bf505097901fa399f40798472c11
Eyupreisoglu <nedimozhaneyupreisoglu@gmail.com>
2022-02-24 00:35:04 +0300

    Practice 2 Updated

--- src/Giris/PracticeKDV.java
+++ src/Giris/PracticeKDV.java
@@ -6,12 +6,16 @@ public class PracticeKDV {
 
         // The KDV prices determined as %18
         Scanner input = new Scanner(System.in);
-        double KDV = 0.18;
+
         System.out.print("Please enter the amount : ");
         double price = input.nextDouble();
 
+        double KDV = (price>1000) ? 0.8 : 0.18;
+
+        double price_kdv = price + (price*KDV);
+
         System.out.println("Without KDV : "+price);
-        System.out.println("Price with KDV : "+ (price+(price*KDV)));
+        System.out.println("Price with KDV : "+ price_kdv);
         System.out.println("Price of KDV: "+ (price*KDV));
 
 
